0b621fef670853ee545d255d29484f6b63abc6e2
Abdelkader0 <Ahmedbob43@gmail.com>
2024-03-16 00:28:56 +0200

    plan section

--- src/Components/Plans/plans.jsx
+++ src/Components/Plans/plans.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
+import {plansData} from '../../data/plansData'
+import whiteTick from '../../assets/whiteTick.png'
 import './plans.css'
 
 const Plans = () => {
   return (
     <div className="plansContainer">
         <div className="plansHeader" style={{gap: '2rem'}}>
-            <span>Ready to Start</span>
+            <span className='stroke-text'>Ready to Start</span>
             <span>Your journy</span>
-            <span>With us</span>
+            <span className='stroke-text'>With us</span>
         </div>
+
+        {/* plans */}
+        <div className="plans">
+        {plansData.map((plans, i)=>(
+          <div className="plan" key={i}>
+            {plans.icon}
+            <span>{plans.name}</span>
+            <span>$ {plans.price}</span>
+
+            {/* planFeatures */}
+            <div className="planFeatures">
+              {plans.features.map((feature, i)=>(
+                <div className="feature">
+                  <img src={whiteTick} alt="" />
+                  <span key={i}>{feature}</span>
+                </div>
+              ))}
+
+              <div>
+                <span>View more details-></span>
+              </div>
+              <button className="btn"> Join now </button>
+            </div>
+            {/* end planFeatures */}
+
+          </div>
+        ))}
+        </div>
+        {/* end plans */}
     </div>
   )
 };
