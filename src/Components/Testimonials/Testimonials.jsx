4deba5d41c88d17df5683cb898133974f79ad876
Abdelkader0 <Ahmedbob43@gmail.com>
2024-03-16 04:00:06 +0200

    testimonials Complete

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Testimonials.css'
+import { testimonialsData } from '../../data/testimonialsData'
+import leftArrow from '../../assets/leftArrow.png'
+import rightArrow from '../../assets/rightArrow.png'
+
 
 const Testimonials = () => {
+    
+    const [selected, setSelected] = useState(0);
+    const tLength = testimonialsData.length;
   return (
-    <div>Testimonials</div>
+    <div className="Testimonials">
+        <div className="left-t">
+            <span>Testimonials</span>
+            <span className='stroke-text'>What they Say</span>
+            <span>AboutUS</span>
+            <span>
+                {testimonialsData[selected].review}
+            </span>
+            <span>
+                <span style={{color: 'var(--orange)'}}>
+                    {testimonialsData[selected].name}
+                </span>{"  "} 
+                -{testimonialsData[selected].status}
+            </span>
+        </div>
+        <div className="right-t">
+            <div></div>
+            <div></div>
+            <img src={testimonialsData[selected].image} alt="" />
+
+            <div className="arrows">
+                <img src={leftArrow} alt="" />
+                <img src={rightArrow} alt="" />
+            </div>
+        </div>
+    </div>
   )
 }
 
