e1699ff4cee72124dbd2d53e299d77fcdb467375
Mariangithubit <metsyb1@hotmail.com>
2024-03-25 03:34:17 +0200

    update for responsive

--- src/Components/Plans/plans.jsx
+++ src/Components/Plans/plans.jsx
@@ -14,30 +14,30 @@ const Plans = () => {
             <span className='stroke-text'>With us</span>
         </div>
 
-        {/* plans */}
+                  {/* plans */}
         <div className="plans">
-        {plansData.map((plans, i)=>(
-          <div className="plan" key={i}>
-            {plans.icon}
-            <span>{plans.name}</span>
-            <span>$ {plans.price}</span>
+          {plansData.map((plans, i)=>(
+            <div className="plan" key={i}>
+              {plans.icon}
+              <span>{plans.name}</span>
+              <span>$ {plans.price}</span>
 
-            {/* planFeatures */}
-            <div className="planFeatures">
-              {plans.features.map((feature, i)=>(
-                <div className="feature">
-                  <img src={whiteTick} alt="" />
-                  <span key={i}>{feature}</span>
-                </div>
-              ))}
-            </div>
+                   {/* planFeatures */}
+              <div className="planFeatures">
+                {plans.features.map((feature, i)=>(
+                  <div className="feature">
+                    <img src={whiteTick} alt="" />
+                    <span key={i}>{feature}</span>
+                  </div>
+                ))}
+              </div>
             {/* end planFeatures */}
-            <div>
+              <div>
                 <span>View more details</span>
               </div>
-              <button className="btn"> Join now </button>
-          </div>
-        ))}
+                <button className="btn"> Join now </button>
+              </div>
+          ))}
         </div>
         {/* end plans */}
     </div>
