a708f690445dab389362bcbeaac319378b660992
Abdelkader0 <Ahmedbob43@gmail.com>
2024-03-29 07:18:45 +0200

    Edit Bmi css done

--- src/Components/Calculate BMI/BmiCalculator.jsx
+++ src/Components/Calculate BMI/BmiCalculator.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-import hero_image from "../../assets/hero_image.png";
 import rightArrow from "../../assets/rightArrow.png";
 import Header from "../Header/Header";
 import "../Header/Header.css";
 import "./BmiCalculator.css";
-import Footer from '../Footer/Footer';
-import Join from '../Join/Join';
-
+import Footer from "../Footer/Footer";
+import Join from "../Join/Join";
 
 function BmiCalculator() {
   const [age, setAge] = useState("");
@@ -56,57 +54,56 @@ function BmiCalculator() {
   return (
     <div className="App">
       <Header />
-      <div className="mainPage">
-        <div className="btn age_gender">
-          <label>Age:</label>
-          <input
-            type="number"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
-          />
-
-          <label>Gender:</label>
-          <select value={gender} onChange={(e) => setGender(e.target.value)}>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
+      <div className="bmi-calculator">
+        <div className="calculator-form">
+          <div className="btn age_gender">
+            <label>Age:</label>
+            <input
+              type="number"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+            <label>Gender:</label>
+            <select value={gender} onChange={(e) => setGender(e.target.value)}>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </div>
+          <div className="btn units">
+            <label>Units:</label>
+            <select value={units} onChange={(e) => setUnits(e.target.value)}>
+              <option value="Metric">Metric</option>
+              <option value="U.S. (Imperial)">U.S. (Imperial)</option>
+            </select>
+          </div>
+          <div className="btn height">
+            <label>Height ({units === "Metric" ? "cm" : "feet/inches"}):</label>
+            <input
+              type="number"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+              required
+            />
+          </div>
+          <div className="btn weight">
+            <label>Weight ({units === "Metric" ? "kg" : "lbs"}):</label>
+            <input
+              type="number"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+              required
+            />
+          </div>
+          <div className="btn calculate">
+            <button onClick={calculateBmi}>
+              Calculate BMI
+              <img src={rightArrow} alt="" />
+            </button>
+            <button onClick={resetValues}>Reset</button>
+          </div>
         </div>
 
-        <div className="btn units">
-          <label>Units:</label>
-          <select value={units} onChange={(e) => setUnits(e.target.value)}>
-            <option value="Metric">Metric</option>
-            <option value="U.S. (Imperial)">U.S. (Imperial)</option>
-          </select>
-        </div>
-        <div className="btn height">
-          <label>Height ({units === "Metric" ? "cm" : "feet/inches"}):</label>
-          <input
-            type="number"
-            value={height}
-            onChange={(e) => setHeight(e.target.value)}
-            required
-          />
-        </div>
-
-        <div className="btn weight">
-          <label>Weight ({units === "Metric" ? "kg" : "lbs"}):</label>
-          <input
-            type="number"
-            value={weight}
-            onChange={(e) => setWeight(e.target.value)}
-            required
-          />
-        </div>
-        <div className="btn calculate">
-          <button onClick={calculateBmi}>
-            Calculate BMI
-            <img src={rightArrow} alt="" />
-          </button>
-          <button onClick={resetValues}>Reset</button>
-        </div>
-
-        <div className="result">
+        <div className="calculator-result">
           <p>
             <strong>BMI Value:</strong> {bmiValue}
           </p>
@@ -115,8 +112,8 @@ function BmiCalculator() {
           </p>
         </div>
       </div>
-        <Join />
-        <Footer />
+      <Join />
+      <Footer />
     </div>
   );
 }
